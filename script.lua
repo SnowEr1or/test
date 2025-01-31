d08254819ad5b22fde9616d095938ffc82788150
SnowEr1or <54193793+SnowEr1or@users.noreply.github.com>
2025-02-01 01:21:22 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,5 @@
+local RunService = game:GetService("RunService")
+
 local Player = game:GetService("Players").LocalPlayer
 local Character = Player.Character
 
@@ -9,16 +11,27 @@ end
 
 local Target = workspace.Live:FindFirstChild(_G.Player)
 
+if not Target then
+	
+	print("Can't find player.")
+	
+end
+
 local TargetHumanoidRootPart = Target:WaitForChild("HumanoidRootPart")
 local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
 local Offset = CFrame.new(-3, 1.5, 3)
 
-local Weld = Instance.new("WeldConstraint")
+local Connection = RunService.Heartbeat:Connect(function()
+	
+	Character:PivotTo(TargetHumanoidRootPart.CFrame * Offset)
+	
+end)
 
-Weld.Parent = Character
+while Character.Parent and Target.Parent do
+	
+	task.wait()
+	
+end
 
-Character:PivotTo(TargetHumanoidRootPart.CFrame * Offset)
-
-Weld.Part0 = HumanoidRootPart
-Weld.Part1 = TargetHumanoidRootPart
+Connection:Disconnect()
