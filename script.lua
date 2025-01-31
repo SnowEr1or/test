1aefe1d95fb17715de75985a0ad0b443c4070746
SnowEr1or <54193793+SnowEr1or@users.noreply.github.com>
2025-02-01 01:14:46 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,24 @@
+local Player = game:GetService("Players").LocalPlayer
+local Character = Player.Character
+
+if not _G.Player then
+	
+	_G.Player = "SnowEr1or"
+	
+end
+
+local Target = workspace:FindFirstChild(_G.Player)
+
+local TargetHumanoidRootPart = Target:WaitForChild("HumanoidRootPart")
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+
+local Offset = CFrame.new(-3, 1.5, 3)
+
+local Weld = Instance.new("WeldConstraint")
+
+Weld.Parent = Character
+
+Character:PivotTo(TargetHumanoidRootPart.CFrame * Offset)
+
+Weld.Part0 = HumanoidRootPart
+Weld.Part1 = TargetHumanoidRootPart
