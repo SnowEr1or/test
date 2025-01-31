2013d7b8c9e9877a02e87b46a289c706b7a4320d
SnowEr1or <54193793+SnowEr1or@users.noreply.github.com>
2025-02-01 01:47:12 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -11,22 +11,70 @@ if not _G.Player then
 	
 end
 
-local Target = workspace.Live:FindFirstChild(_G.Player)
+if RunService:IsStudio() then
+	
+	_G.Player = "Rig"
+	
+end
 
-if not Target then
+local Target = nil
+local TargetPlayer: Player? = game:GetService("Players"):FindFirstChild(_G.Player)
+
+if workspace:FindFirstChild("Live") then
+	
+	Target = workspace.Live:FindFirstChild(_G.Player)
+	
+else
+	
+	Target = workspace:FindFirstChild(_G.Player)
+	
+end
+
+if (not Target or not TargetPlayer) and not RunService:IsStudio() then
 	
 	print("Can't find player.")
+	
+	return
 	
 end
 
 local TargetHumanoidRootPart = Target:WaitForChild("Torso")
 local HumanoidRootPart = Character:WaitForChild("Torso")
 
-local Offset = CFrame.new(-3, 1.5, 3)
+local Debounce = false
+
+local NormalOffset = CFrame.new(-3, 1.5, 3)
+local Offset = NormalOffset
 
 local Connection = RunService.Heartbeat:Connect(function()
 	
 	Character:PivotTo(TargetHumanoidRootPart.CFrame * Offset)
+	
+end)
+
+local Chatted = TargetPlayer.Chatted:Connect(function(Message)
+	
+	if Debounce then
+		
+		return
+		
+	end
+	
+	Message = string.lower(Message)
+	
+	if Message == "1" then
+		
+		Debounce = true
+		
+		Offset = CFrame.new(0, 0.5, -5)
+		
+		task.wait(1)
+		
+		Offset = NormalOffset
+		
+		Debounce = false
+		
+	end
 	
 end)
 
@@ -37,3 +85,4 @@ while Character.Parent and Target.Parent do
 end
 
 Connection:Disconnect()
+Chatted:Disconnect()
